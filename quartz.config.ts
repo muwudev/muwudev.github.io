59fbffcf2a57acf1a665b515f9fb80a46e7fb7b6
muwudev <cavitieshurt@gmail.com>
2025-02-06 23:27:15 +0530

    Quartz sync: Feb 6, 2025, 11:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Amna K.",
+    pageTitle: "amna's brain dump",
+
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
